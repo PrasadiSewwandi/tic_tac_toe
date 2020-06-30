7765eb12bbf9966eba77334f8a7e2c3bc22a8bd2
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-06-30 12:25:29 +0530

    next step done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM, { render } from 'react-dom'
 
 class Square extends React.Component {
   render() {
@@ -15,4 +15,55 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square />;
   }
+
+
+render() {
+  const status = 'Next player:X' ;
+
+return (
+  <div>
+    <div className="status">{status}</div>
+    <div className = "bosrd-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+    </div>
+  </div>
+  );
+ }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className = "game">
+        <div className = "game-board">
+          <Board/>
+        </div>
+        <div className ="game-info">
+          <Board/>
+          </div>
+          <div className = "game-info">
+            <div>{/* status */}</div>
+            <ol>{/* ToDo*/}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+// ==========================================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
